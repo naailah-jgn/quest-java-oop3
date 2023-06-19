a295b5bb1a86bd0ac42b3335c6fa80ea0e2ae742
Naailah JUGON <jugonnaailah@gmail.com>
2023-06-19 11:50:21 +0200

    finalized challenge poo3

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,40 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.println("Hawkeye takes off in the sky.");
+    }
+
+    @Override
+    public void ascend(int altitude) {
+        this.altitude += altitude;
+        System.out.println("Hawkeye flies upward, altitude: " + this.altitude);
+    }
+
+    @Override
+    public void descend(int altitude) {
+        if (this.altitude - altitude > 0) {
+            this.altitude -= altitude;
+            System.out.println("Hawkeye flies downward, altitude: " + this.altitude);
+        } else {
+            System.out.println("Hawkeye is too high, it can't land.");
+        }
+    }
+
+    @Override
+    public void glide() {
+        System.out.println("Hawkeye glides into the air.");
+    }
+
+    @Override
+    public void land() {
+        if (altitude > 0) {
+            System.out.println("Hawkeye lands on the ground.");
+        } else {
+            System.out.println("Hawkeye is already on the ground.");
+        }
     }
 }
---
+++ Fly.java
@@ -0,0 +1,7 @@
+public interface Fly {
+    void takeOff();
+    void ascend(int altitude);
+    void descend(int altitude);
+    void glide();
+    void land();
+}
